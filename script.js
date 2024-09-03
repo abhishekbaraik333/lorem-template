6b2ceefdaed910b1bbd0288bbc65f84875894c57
abhishekbaraik333 <abhitamangme@gmail.com>
2024-09-04 01:35:22 +0530

    second

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileNav.classList.toggle('active');
 });
+
+
+// swiper js
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true, // Enables looping
+    autoplay: {
+        delay: 3000, // Delay between slides in milliseconds (3 seconds)
+        disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
