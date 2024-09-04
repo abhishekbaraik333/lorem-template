79540c714b2b02464f2d8ec5f0e2c016824ef375
abhishekbaraik333 <abhitamangme@gmail.com>
2024-09-04 17:36:59 +0530

    all done

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const hamburger = document.getElementById('hamburger');
-const mobileNav = document.getElementById('mobile-nav');
+const hamburger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobile-nav");
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    mobileNav.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileNav.classList.toggle("active");
 });
 
-
 // swiper js
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    loop: true, // Enables looping
-    autoplay: {
-        delay: 3000, // Delay between slides in milliseconds (3 seconds)
-        disableOnInteraction: false, // Continue autoplay after user interaction
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true, // Enables looping
+  autoplay: {
+    delay: 3000, // Delay between slides in milliseconds (3 seconds)
+    disableOnInteraction: false, // Continue autoplay after user interaction
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-    breakpoints: {
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    },
+  },
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item");
+
+  faqItems.forEach((item) => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+      item.classList.toggle("active"); // Toggle clicked item
+    });
+  });
 });
